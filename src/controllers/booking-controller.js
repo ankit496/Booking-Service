d5dab230ade0169ccfcc4685d943b6b8364b46e5
ankit496 <akankit496@gmail.com>
2023-11-16 11:15:44 +0530

    minor bug fix

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -10,8 +10,16 @@ class BookingController{
     }
     async sendMessageToQueue(req,res){
         const channel=await createChannel()
-        const data={message:'Success'}
-        publishMessage(channel,REMINDER_BINDING_KEY,JSON.stringify(data))
+        const payload={
+            data:{
+                subject:'This is a notification from queue',
+                content:'Some queue will subscribe this',
+                recepientEmail:'akankit3020@gmail.com',
+                notificationTime: '2023-11-16 10:48:34'
+            },
+            service:'CREATE_TICKET'
+        }
+        publishMessage(channel,REMINDER_BINDING_KEY,JSON.stringify(payload))
         return res.status(200).json({
             message:'Successfully published the event'
         })
